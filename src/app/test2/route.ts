e4036d3b51c6262dbbcd59f4a325a5b036af3787
Jamie Dubs <jamie@jamiedubs.com>
2023-09-19 12:06:47 -0400

    Add indicators to logs to know which endpoint is which

--- src/app/test2/route.ts
+++ src/app/test2/route.ts
@@ -2,11 +2,11 @@ export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  console.log("Start");
+  console.log("Start GET test2");
 
   await new Promise((resolve) => setTimeout(resolve, 22_000));
 
-  console.log("Before return");
+  console.log("Before return GET test2");
 
   return Response.json({ foo: "bar" });
 }
--- src/app/test3/route.ts
+++ src/app/test3/route.ts
@@ -2,11 +2,11 @@ export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  console.log("Start");
+  console.log("Start GET test3");
 
   await new Promise((resolve) => setTimeout(resolve, 10_000));
 
-  console.log("Before return");
+  console.log("Before return GET test3");
 
   return Response.json({ foo: "bar" });
 }
